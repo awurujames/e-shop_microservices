a31bf1f080bf32d877c54c7a1746ccf53329e84e
James Awuru <James.Awuru@Interswitchng.com>
2025-01-20 09:01:04 +0100

    initial commit

---
+++ Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, List<string>Category, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+
+
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business logic to create a Product
+            throw new NotImplementedException();
+        }
+    }
+}
